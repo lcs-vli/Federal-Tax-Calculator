e74713bf6df9dd21ef386770c2db1359d2e09087
Victoria Li <VLi@lcs.on.ca>
2019-10-31 20:17:47 -0400

    Initial Commit

---
+++ Federal Tax Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Federal Tax Calculator
+//
+//  Created by Li, Muchen on 2019-10-31.
+//  Copyright © 2019 Li, Muchen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
