eeda3598d3a1fd966db2d08b77f624a9d09c7752
Victoria Li <VLi@lcs.on.ca>
2019-11-01 21:15:45 -0400

    can calculate tax owning and tax rate

--- Federal Tax Calculator/ViewController.swift
+++ Federal Tax Calculator/ViewController.swift
@@ -26,50 +26,48 @@ class ViewController: UIViewController {
     }
     
     func calculate(){
-        //get user input
-        guard let userName = name.text else {
-            taxOwing.text = "Please enter your first name"
-            return
-        }
-        guard let annualIncomeText = annualIncome.text else {
-            taxOwing.text = "Please enter your annual income in dollars"
-            return
-        }
-                  
-        //convert all values into doubl
-        guard let Name = Double(userName) else {
-            return
-        }
-        guard let Income = Double(annualIncomeText) else {
-            return
-        }
         
+    //get user input
+    guard let userName = name.text else {
+        taxOwing.text = "Please enter your first name"
+        return
+    }
+    guard let annualIncomeText = annualIncome.text else {
+        taxOwing.text = "Please enter your annual income in dollars"
+        return
+    }
+                              
+    //convert value into double
+    guard let Income = Double(annualIncomeText) else {
+        return
+    }
+    
         //calculate tax owing
         switch Income{
         case 0...47630:
-            let owing = round(1.15 * Income)
+            let owing = round(0.15 * Income)
             let rateOfTax = round(owing/Income*100)
-            taxOwing.text = "\(Name) your federal tax owing is $ \(owing)"
+            taxOwing.text = "\(userName) your federal tax owing is $ \(owing)"
             taxRate.text = "Effective tax rate is: \(rateOfTax)%"
         case 47631...95259:
-            let owing = round(1.15 * 47630 + 1.205*(Income - 47630))
+            let owing = round(0.15 * 47630 + 0.205*(Income - 47630))
             let rateOfTax = round(owing/Income*100)
-            taxOwing.text = "\(Name) your federal tax owing is $ \(owing)"
+            taxOwing.text = "\(userName) your federal tax owing is $ \(owing)"
             taxRate.text = "Effective tax rate is: \(rateOfTax)%"
         case 95260...147667:
-            let owing = round(1.15 * 47630 + 1.205 * 47629 + 1.26 * (Income - 95259))
+            let owing = round(0.15 * 47630 + 0.205 * 47629 + 0.26 * (Income - 95259))
             let rateOfTax = round(owing/Income*100)
-            taxOwing.text = "\(Name) your federal tax owing is $ \(owing)"
+            taxOwing.text = "\(userName) your federal tax owing is $ \(owing)"
             taxRate.text = "Effective tax rate is: \(rateOfTax)%"
-        case 147,668...210,371:
-            let owing = round(1.15 * 47630 + 1.205 * 47629 + 1.26 * 52408 + 1.29 * (Income-147667))
+        case 147668...210371:
+            let owing = round(0.15 * 47630 + 0.205 * 47629 + 0.26 * 52408 + 0.29 * (Income-147667))
             let rateOfTax = round(owing/Income*100)
-            taxOwing.text = "\(Name) your federal tax owing is $ \(owing)"
+            taxOwing.text = "\(userName) your federal tax owing is $ \(owing)"
             taxRate.text = "Effective tax rate is: \(rateOfTax)%"
         default:
-            let owing = round(1.15*47630 + 1.205*47629 + 1.26*52408 + 1.29*62704 + 1.33*(Income-210371))
+            let owing = round(0.15*47630 + 0.205*47629 + 0.26*52408 + 0.29*62704 + 0.33*(Income-210371))
             let rateOfTax = round(owing/Income*100)
-            taxOwing.text = "\(Name) your federal tax owing is $ \(owing)"
+            taxOwing.text = "\(userName) your federal tax owing is $ \(owing)"
             taxRate.text = "Effective tax rate is: \(rateOfTax)%"
         }
         
